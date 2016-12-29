910826d85dfa5d03ff185c538cd44086e54bd455
Ordanis <ordanisanchez@gmail.com>
2016-12-28 23:59:08 -0400

    Add templates folder, update main view to return base

--- app/views/main_view.py
+++ app/views/main_view.py
@@ -1,9 +1,11 @@
+from flask import render_template
 from flask_classy import FlaskView
 
 
 class Main(FlaskView):
     """ Main page view. """
+
     route_base = "/"
 
     def index(self):
-        return "HEEEEYYYYY"
+        return render_template("base.html")
