67a02addf9ae1255f015b3ff9d8cf3af72704eab
Ordanis <ordanisanchez@gmail.com>
2017-02-08 18:16:29 -0400

    Add helpersjs with some initial template

---
+++ app/static/js/helpers.js
@@ -0,0 +1,8 @@
+var commentsTemplate = _.template(
+  '<div class="card">\
+    <div class="card-block">\
+      <h5 class="card-title"><% user %></h5>\
+      <p class="card-text"><%= comment %></p>\
+    </div>\
+  </div>'
+);
