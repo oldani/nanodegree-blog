8c2560e3ff374b4068733bd079e5402232f51a0c
Ordanis <ordanisanchez@gmail.com>
2017-02-09 13:57:27 -0400

    Add submit comments feature

--- app/static/js/comments.js
+++ app/static/js/comments.js
@@ -1,38 +1,72 @@
-var model = {
-  init: function(){
-    var _self = this;
-    var url = window.allCommentEndpoint || '';
-    return $.getJSON(url).done(function(data) {
-      _self.comments = data;
-    });
-  },
-  comments: []
-};
+(function() {
+  var model = {
+    init: function(){
+      var url = window.allCommentEndpoint || '';
+      return $.getJSON(url).done(function(data) {
+        this.comments = data;
+      }.bind(this));
+    },
+    comments: []
+  };
 
-var controller = {
-  init: function() {
-    model.init().then(function() {
-      view.init();
-    });
-  },
-  getComments: function(){
-    return model.comments;
-  }
-};
+  var controller = {
+    init: function() {
+      model.init().then(function() {
+        commentsView.init();
+      });
+      formView.init();
+    },
+    getComments: function(){
+      return model.comments;
+    },
+    updateComments: function(comment) {
+      model.comments = model.comments.concat(comment);
+      commentsView.reRender();
+    }
+  };
 
-var view = {
-  init:function() {
-    this.commentsContainer = $("#comments");
-    this.commentsTemplate = commentsTemplate;
-    this.comments = controller.getComments();
-    this.render();
-  },
-  render: function() {
-    _.each(this.comments, function(item, i) {
-      var commet = this.commentsTemplate(item);
-      this.commentsContainer.append(commet)
-    }.bind(this));
-  }
-};
+  var commentsView = {
+    init: function() {
+      this.$commentsContainer = $(".comments");
+      this.commentsTemplate = commentsTemplate;
+      this.comments = controller.getComments();
+      this.render();
+    },
+    render: function() {
+      this.$commentsContainer.html('');
+      _.each(this.comments, function(item, i) {
+        var comment = this.commentsTemplate(item);
+        this.$commentsContainer.append(comment);
+      }.bind(this));
+      this.$commentsContainer.fadeIn("slow");
+    },
+    reRender: function() {
+      this.$commentsContainer.fadeOut("slow");
+      this.comments = controller.getComments();
+      this.render();
+    }
+  };
 
-controller.init()
+  var formView = {
+    init: function(){
+      var postEnpoint = window.commentPostEnpoint || '';
+      var form = $(".comment-form form");
+
+      form.submit(function(e) {
+        e.preventDefault();
+
+        var $this, data;
+
+        $this = $(this);
+        data = $this.serializeArray();
+
+        $.post(postEnpoint, data).done(function(data) {
+          $this.find("#comment").val("");
+          controller.updateComments(data);
+        });
+      });
+    },
+  };
+
+  controller.init();
+})();
