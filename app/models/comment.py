9e01f2af77b45e7a2b728626bce89ded2bea55bf
Ordanis <ordanisanchez@gmail.com>
2017-02-10 14:40:23 -0400

    Extends put and update in comment model

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from .base import BaseModel
 
 
@@ -7,3 +8,15 @@ class Comment(BaseModel):
     def __init__(self, **kwargs):
         self.kind = "Comment"
         super().__init__(self.kind, **kwargs)
+
+    def put(self):
+        """ Extends put method to add some extra fields before saving. """
+        self.data['created'] = datetime.now()
+        self.data['updated'] = datetime.now()
+        super().put()
+
+    def update(self):
+        """ Extends update method to update some fields before saving. """
+        if self.data.get('updated'):
+            self.data['updated'] = datetime.now()
+        super.update(self.data.get("id"), self.data)
