65e54a17cda497da6ad5fdee890839d2380d778c
Ordanis <ordanisanchez@gmail.com>
2017-02-10 18:59:22 -0400

    Add delete feature in the fron end

--- app/static/js/comments.js
+++ app/static/js/comments.js
@@ -22,30 +22,62 @@
     updateComments: function(comment) {
       model.comments = [comment].concat(model.comments);
       commentsView.reRender();
+    },
+    deleteComment: function(commentId) {
+      model.comments = _.reject(model.comments, function(comment) {
+        return comment.id == commentId;
+      });
     }
   };
 
   var commentsView = {
     init: function() {
       this.currentUser = window.currentUser;
+      this.deleteEndpoint = window.commentDeleteEndpoint;
+
       this.$commentsContainer = $(".comments");
       this.commentsTemplate = commentsTemplate;
+
       this.comments = controller.getComments();
+
       this.render();
+
+      this.$commentsContainer.on('click', '.delete-comment', function(e) {
+        e.preventDefault();
+        this.deleteComment(e.target.href);
+      }.bind(this));
     },
     render: function() {
       this.$commentsContainer.html('');
       _.each(this.comments, function(item, i) {
         item.currentUser = this.currentUser;
+        item.deleteUrl = this.deleteEndpoint.concat(item.id);
+
         var comment = this.commentsTemplate(item);
         this.$commentsContainer.append(comment);
+
       }.bind(this));
+
       this.$commentsContainer.fadeIn("slow");
     },
     reRender: function() {
       this.$commentsContainer.fadeOut("slow");
       this.comments = controller.getComments();
       this.render();
+    },
+    deleteComment: function(targetUrl) {
+      var postId = _.last(window.commentPostEnpoint.split('/'));
+      $.ajax(targetUrl, {
+        method: "DELETE",
+        data: {'post_id': postId}
+      })
+      .done(function(){
+        commentId = _.last(targetUrl.split("/"));
+
+        controller.deleteComment(commentId);
+        this.reRender();
+
+      }.bind(this));
     }
   };
 
