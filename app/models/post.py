1f20ae45e8c0c31ceb396630405bf718c9ded60b
Ordanis <ordanisanchez@gmail.com>
2017-02-11 11:15:49 -0400

    Add delete comment view, and delete_comment func to post model

--- app/models/post.py
+++ app/models/post.py
@@ -9,10 +9,17 @@ class Post(BaseModel):
         self.comment_list = self.data.get("comment_list")
 
     def add_comment(self, comment_id):
+        """ Append a comment id to a comment_list field. """
         if not self.comment_list:
             self.comment_list = self.data["comment_list"] = []
         self.comment_list.append(comment_id)
         self.update()
+
+    def delete_comment(self, comment_id):
+        """ Delete a given comment id from commnet list. """
+        # self.comment_list.remove(comment_id)
+        # self.update()
+        pass
 
     def update(self, entity_id=None, data=None):
         if not entity_id and not data:
--- app/views/comment_view.py
+++ app/views/comment_view.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask_classy import FlaskView
 from flask_user import current_user, login_required
 from ..models import CommentModel, PostModel
@@ -29,3 +29,16 @@ class Comment(FlaskView):
             post.add_comment(comment.id)
             return jsonify(comment.data)
         return "form.errors"
+
+    @login_required
+    def delete(self, comment_id):
+        post_id = request.form.get('post_id')
+        if post_id:
+            # CommentModel().delete(comment_id)
+            # post = PostModel().get(post_id)
+            # post.delete_comment(comment_id)
+            return "", 200
+        return "hola"
+        # comment = CommentModel()
+        # comment = comment.get(post_id)
+        # print(comment)
