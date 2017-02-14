4976c35ae6339bbf2a28835a93aeea4f79a8b2ae
Ordanis <ordanisanchez@gmail.com>
2017-02-14 00:08:19 -0400

    Add comment list as defualt when create a post, extend delete class method

--- app/models/post.py
+++ app/models/post.py
@@ -1,7 +1,13 @@
 from .base import BaseModel
+from flask_user import current_user
 
 
 class Post(BaseModel):
+
+    def __init__(self, **kwargs):
+        # Add a comment list field
+        self.comment_list = []
+        super().__init__(**kwargs)
 
     def add_comment(self, comment_id):
         """ Append a comment id to a comment_list field or
@@ -15,3 +21,15 @@ class Post(BaseModel):
         """ Delete a given comment id from commnet list. """
         self.comment_list.remove(int(comment_id))
         self.update()
+
+    @classmethod
+    def delete(cls, entity_id):
+        post = cls.get(entity_id)
+        if post:
+            if post.comment_list:
+                cls.delete_multi(post.comment_list, kind='Comment')
+            super().delete(entity_id)
+            current_user.posts_list.remove(int(entity_id))
+            current_user.put()
+            return True
+        return
