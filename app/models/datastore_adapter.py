0523fe4d45bcdcae79e1bc938a48f9bf1983aac2
Ordanis <ordanisanchez@gmail.com>
2017-02-16 17:18:41 -0400

    Fix bug in datastore adapter in first_object func

--- app/models/datastore_adapter.py
+++ app/models/datastore_adapter.py
@@ -28,7 +28,7 @@ class DataStoreAdapter(DBAdapter):
         for field, value in kwargs.items():
             ObjectClass.add_query_filter(field, "=", value)
         entity = ObjectClass.fetch(limit=1)
-        return entity
+        return entity[0]
 
     def ifind_first_object(self, ObjectClass, **kwargs):
         """ Retrieve the first Entity matching the filters in
@@ -38,7 +38,7 @@ class DataStoreAdapter(DBAdapter):
         for field, value in kwargs.items():
             ObjectClass.add_query_filter(field, "=", value)
         entity = ObjectClass.fetch(limit=1)
-        return entity
+        return entity[0]
 
     def add_object(self, ObjectClass, **kwargs):
         """ Create an Entity with the fields specified in kwargs. """
